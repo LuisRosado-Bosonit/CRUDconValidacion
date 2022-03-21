073e1005b12204949b3ec6b350a5b7736f271bcc
luis.rosado <luis.rosado@bosonit.com>
2022-03-21 18:15:31 +0100

    Implementada la generación automatica de secuencias en la entidad Student.java

--- CRUDconValidacion/src/main/java/Student/domain/Student.java
+++ CRUDconValidacion/src/main/java/Student/domain/Student.java
@@ -1,16 +1,33 @@
 package Student.domain;
 
+import Student.Utils.StringPrefixedSequenceIdGenerator;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.hibernate.annotations.GenericGenerator;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
+@EqualsAndHashCode
 @Data
 public class Student {
     @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "ausencias_seq")
+    @GenericGenerator(
+            name = "ausencias_seq",
+            strategy = "src/main/java/Student/Utils/StringPrefixedSequenceIdGenerator.java",
+            parameters = {
+                    @org.hibernate.annotations.Parameter(name = StringPrefixedSequenceIdGenerator.INCREMENT_PARAM, value = "1"),
+                    @org.hibernate.annotations.Parameter(name = StringPrefixedSequenceIdGenerator.VALUE_PREFIX_PARAMETER, value =
+                            "AUS"),
+                    @org.hibernate.annotations.Parameter(name = StringPrefixedSequenceIdGenerator.NUMBER_FORMAT_PARAMETER, value =
+                            "%08d")
+            } )
     @Column(name = "id_student", nullable = false)
     private String id_student;
+
+    @Column
+            //@OneToOne(mappedBy = "")
+    String id_persona;
 
 }
