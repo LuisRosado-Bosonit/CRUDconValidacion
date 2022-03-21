33c8fd00cb4844757838afcb6b5fde2ea54237b8
luis.rosado <luis.rosado@bosonit.com>
2022-03-21 18:01:57 +0100

    Creada clase Student.java que contendrá a la entidad Student en el futuro, y añadida la clase para generar secuencias de tipo String ( StringPrefixedSequenceIdGenerator )

---
+++ CRUDconValidacion/src/main/java/Student/domain/Student.java
@@ -0,0 +1,16 @@
+package Student.domain;
+
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+@Data
+public class Student {
+    @Id
+    @Column(name = "id_student", nullable = false)
+    private String id_student;
+
+}
