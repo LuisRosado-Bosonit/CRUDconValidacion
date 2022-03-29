7b4da30c6273fd8555af3900b17978aaf7f44f01
luis.rosado <luis.rosado@bosonit.com>
2022-03-29 17:19:34 +0200

    Incorporación de swagger

--- CRUDconValidacion/src/main/java/App/CruDconValidacionApplication.java
+++ CRUDconValidacion/src/main/java/App/CruDconValidacionApplication.java
@@ -3,10 +3,15 @@ package App;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
 import org.springframework.web.client.RestTemplate;
+import springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 @SpringBootApplication
 //@EnableFeighClients
+@EnableSwagger2WebMvc
+@Import(SpringDataRestConfiguration.class)
 public class CruDconValidacionApplication {
 
 	public static void main(String[] args) {
